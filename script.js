843ec6fc158bee0addf2c627d1f27fa8f8e3826a
Elisariane <elisarianeBS@gmail.com>
2022-11-07 09:01:11 -0300

    Adicionado anotações da aula de tipos de dados

---
+++ script.js
@@ -0,0 +1,19 @@
+const a = 10 //number
+const b = 10.5 //number
+const c = 'Maria' // string
+const d = 'a' // string
+const e = false // boolean
+const f = {name: 'Maria', age: 25} // array
+const g = ['a', 15, false, 'maria'] // object
+const h = null //object
+const i = undefined //object
+
+console.log( a + " - tipo:" + typeof a)
+console.log( b + " - tipo:" +typeof b)
+console.log( c + " - tipo:" +typeof c)
+console.log( d + " - tipo:" +typeof d)
+console.log( e + " - tipo:" +typeof e)
+console.log( f + " - tipo:" +typeof f)
+console.log( g + " - tipo:" +typeof g)
+console.log( h + " - tipo:" +typeof h)
+console.log( i + " - tipo:" +typeof i)
